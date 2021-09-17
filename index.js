5b6734202b50a67c669e06e5eb44d109f155b7a2
joaquim-almeida <joaquimrafael30@hotmail.com>
2021-09-16 22:55:56 -0300

    adicionado partials no projeto

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
 //configurando template engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+//config body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.get("/:nome/:lang", (req, res) => {
-    let nome = req.params.nome
-    let lang = req.params.lang
-    let exibirMsg = false
+//routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
-    let produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Chá de pessego", preco: 5.99},
-        {nome: "Coxinha", preco: 0.50}
-    ]
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+});
 
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Company ltda",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    })
-})
+app.post("/salvarpergunta", (req, res) => {
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send("formulário recebido! \nTitulo: " + titulo + " Descricao: " + descricao);
+});
 
 app.listen(3443, () => {
-    console.log("App rodando em http://localhost:3443")
-})
+    console.log("App rodando em http://localhost:3443");
+});
